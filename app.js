aab8f88f09948642f3ede325b27bdafe6a945136
jacque_l <jacquelinlouis@gmail.com>
2017-08-02 11:47:26 +0200

    Add internationalization module i18n

--- app.js
+++ app.js
@@ -1,25 +1,40 @@
 /**
  * Created by Louis on 29/07/2017.
  */
+
+// express
 var express = require('express');
 var app = express();
+// internationalization
+/*
+var config = {
+    "lang": "en",
+    "langFile": "./internationalization/locale.json"
+};
+*/
+var i18n = require('i18n');
+i18n.configure({
+   locales:['en', 'fr'],
+    directory: __dirname + '/locales'
+});
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/views'));
+app.use(i18n.init);
 
 app.get('/', function (req, res) {
     res.status(200);
-    res.render('pages/working');
+    res.render('pages/working.ejs');
 });
 
 app.get('/about', function (req, res) {
     res.status(200);
-    res.render('pages/about');
+    res.render('pages/about.ejs', { i18n: i18n});
 });
 
 app.get('/blog', function (req, res) {
     res.status(200);
-    res.render('pages/working');
+    res.render('pages/working.ejs');
 });
 
 app.get('/contact', function (req, res) {
@@ -29,12 +44,12 @@ app.get('/contact', function (req, res) {
 
 app.get('/projects', function (req, res) {
     res.status(200);
-    res.render('pages/working');
+    res.render('pages/working.ejs');
 });
 
 app.use(function(req, res, next) {
     res.status(404);
-    res.render('pages/notFound');
+    res.render('pages/notFound.ejs');
 });
 
 app.listen(8080);
